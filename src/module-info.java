6a89ded8d50f3308125176e3d2e6ec0b0fb3b092
Ayoub <ubmagh@gmail.com>
2022-01-23 14:06:34 +0100

    Changed .so linux files to .dll win files

--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,7 @@
 module RTCA {
     requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.base;
     requires javafx.fxml;
     requires mongo.java.driver;
     requires spring.security.crypto;
