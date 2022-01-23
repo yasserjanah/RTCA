5743ccde3445ac34a90fe23b0e4690416695ab8b
Ayoub <ubmagh@gmail.com>
2022-01-23 14:26:02 +0100

    Added views/login_rtca.fxml # there is a  path error

--- src/GUI/GUIApplication.java
+++ src/GUI/GUIApplication.java
@@ -1,28 +1,39 @@
 package GUI;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class GUIApplication extends Application {
+
+    private static Stage primaryStageObj;
+
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        //creating a Group object
-        Group group = new Group();
+
+        primaryStageObj = primaryStage;
+
+        //creating the root object
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("/Views/login_RTCA.fxml"));
+
 
         //Creating a Scene by passing the group object, height and width
-        Scene scene = new Scene(group ,600, 300);
-
-        //setting color to the scene
-        scene.setFill(Color.BROWN);
+        Scene scene = new Scene(root ,1080, 720);
+        scene.setRoot(root);
 
         //Setting the title to Stage.
-        primaryStage.setTitle("Sample Application");
+        primaryStage.setTitle("RTCA fx-client");
 
         //Adding the scene to Stage
         primaryStage.setScene(scene);
+
+        //disable resizing
+        primaryStage.setResizable(false);
 
         //Displaying the contents of the stage
         primaryStage.show();
@@ -30,4 +41,9 @@ public class GUIApplication extends Application {
     public static void main(String args[]){
         launch(args);
     }
+
+    public static Stage getPrimaryStage() {
+        return primaryStageObj;
+    }
+
 }
